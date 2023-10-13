924e81374bd9fca962d8a3ff327919e9463d7bc1
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-10-13 19:00:29 -0300

    Navbar component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,18 +5,21 @@ import { CartItemControlComponent } from "./cart-item/cart-item-control.componen
 import { CartWidgetComponent } from "./cart-widget/cart-widget.component";
 import { ProductsListComponent } from "./product-list/products-list.component";
 import { CoreModule } from "../core/core.module";
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @NgModule({
   declarations: [
     ProductsListComponent,
     CartWidgetComponent,
     CartItemControlComponent,
+    NavbarComponent,
   ],
   imports: [BrowserModule, CoreModule],
   exports: [
     ProductsListComponent,
     CartWidgetComponent,
     CartItemControlComponent,
+    NavbarComponent
   ]
 })
 export class ComponentsModule {}
